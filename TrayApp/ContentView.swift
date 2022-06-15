3dbefc7c37eb0829b1828d7855ca29b7478f6534
Jon Crawley <jonccrawley@me.com>
2022-06-15 11:19:22 -0600

    init commit

--- TrayApp/ContentView.swift
+++ TrayApp/ContentView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        ZStack {
+            Color(red: 0, green: 0.251, blue: 0.322)
+            VStack(alignment: .leading) {
+                Button(action: {
+                    startStream()
+                }, label: {
+                   Text("Stream System audio")
+                       .foregroundColor(Color.white)
+                       .cornerRadius(5)
+                })
+
+            }
+        }
+
     }
 }
 
+func startStream(){
+    print("Stream Audio.....")
+    
+    //https://developer.apple.com/documentation/screencapturekit/capturing_screen_content_in_macos
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
