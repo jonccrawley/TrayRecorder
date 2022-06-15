39bc1f231d702749884e26b0ef20f2f16a3dab48
Jon Crawley <jon.crawley@jumpcloud.com>
2022-06-15 09:29:50 -0600

    Initial Commit

---
+++ TrayApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TrayApp
+//
+//  Created by Jon Crawley on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
